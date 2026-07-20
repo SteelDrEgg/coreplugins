6df8eb70db5de2ee15e46caf45880b3ac1af3b61
DrEgg <102451227+SteelDrEgg@users.noreply.github.com>
2026-07-20 20:11:31 +0800

    Adopt SDK

--- coreplugins/webassets/main.go
+++ coreplugins/webassets/main.go
@@ -3,74 +3,25 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-
-	panel "github.com/SteelDrEgg/coreplugins/pluginsdk/wasm/proto"
+	arupa "github.com/SteelDrEgg/arupa-sdk/golang"
+	"github.com/SteelDrEgg/arupa-sdk/golang/wasm"
 )
 
 func main() {}
 
 func init() {
-	panel.RegisterPlugin(webAssetsPlugin{})
-}
-
-type webAssetsPlugin struct{}
-
-const webAssetsNamespace = "web-assets"
-
-func (webAssetsPlugin) Register(ctx context.Context, _ *panel.RegisterRequest) (*panel.RegisterReply, error) {
-	host := panel.NewHost()
-	urls := map[string]string{
-		"style":        "/assets/css/daisyui@5.css",
-		"color-scheme": "/assets/css/scheme.css",
-		"icon":         "/assets/icon/",
-		"culori":       "/assets/vendor/culori.js",
-	}
-	urlsJSON, err := json.Marshal(urls)
-	if err == nil {
-		_, _ = host.KVSet(ctx, &panel.KVSetRequest{
-			Namespace: webAssetsNamespace,
-			Key:       "urls",
-			Value:     urlsJSON,
-		})
-	}
-	for k, v := range urls {
-		_, _ = host.KVSet(ctx, &panel.KVSetRequest{
-			Namespace: webAssetsNamespace,
-			Key:       k,
-			Value:     []byte(v),
-		})
-	}
-
-	return &panel.RegisterReply{
-		Name:    "web-assets",
-		Version: pluginVersion,
-		StaticMounts: []*panel.StaticMount{
-			{
-				Prefix:    "/assets/css/",
-				Directory: "$PLUGIN_ROOT/assets/css",
-			},
-			{
-				Prefix:    "/assets/icon/",
-				Directory: "$PLUGIN_ROOT/assets/icon",
-			},
-			{
-				Prefix:    "/assets/vendor/",
-				Directory: "$PLUGIN_ROOT/assets/vendor",
+	plugin := &wasm.Plugin{
+		Registration: arupa.Registration{
+			Name:    webAssetsNamespace,
+			Version: pluginVersion,
+			StaticMounts: []arupa.StaticMount{
+				{Prefix: "/assets/css/", Directory: "$PLUGIN_ROOT/assets/css"},
+				{Prefix: "/assets/icon/", Directory: "$PLUGIN_ROOT/assets/icon"},
+				{Prefix: "/assets/vendor/", Directory: "$PLUGIN_ROOT/assets/vendor"},
 			},
 		},
-	}, nil
+	}
+	wasm.Register(plugin)
 }
 
-func (webAssetsPlugin) HandleHTTP(_ context.Context, _ *panel.HTTPRequest) (*panel.HTTPResponse, error) {
-	return &panel.HTTPResponse{Status: 404}, nil
-}
-
-func (webAssetsPlugin) HandleSocketEvent(_ context.Context, _ *panel.SocketEvent) (*panel.SocketEventReply, error) {
-	return &panel.SocketEventReply{}, nil
-}
-
-func (webAssetsPlugin) HandlePluginMessage(_ context.Context, _ *panel.PluginMessage) (*panel.PluginMessageReply, error) {
-	return &panel.PluginMessageReply{}, nil
-}
+const webAssetsNamespace = "web-assets"
